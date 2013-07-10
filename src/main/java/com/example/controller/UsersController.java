c482a0a0ab0cdddbb6ca18cc5444e10c2bdaf53a
Justin Culbertson <jjculber@calpoly.edu>
2013-07-09 18:54:29 -0700

    Fixed TimeZone bug and moved database credentials to uncommitted file.

--- src/main/java/com/example/controller/UsersController.java
+++ src/main/java/com/example/controller/UsersController.java
@@ -18,6 +18,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Collections;
 import java.util.Calendar;
+import java.util.TimeZone;
 
 @Controller
 public class UsersController {
